6f1e7d0d2cd489eb0a215142d0c726b1e68bbf6f
Per Nordlöw <per.nordlow@gmail.com>
2021-08-20 00:17:31 +0200

    Add disposeX

--- src/dsymbol/makex.d
+++ src/dsymbol/makex.d
@@ -13,3 +13,40 @@ auto makeX(T, Allocator, A...)(auto ref Allocator alloc, auto ref A args)
     else
         return make!(T)(alloc, args);
 }
+
+void disposeX(A, T)(auto ref A alloc, auto ref T* p)
+{
+    version(safeAlloc)
+    {
+    }
+    else
+    {
+        import stdx.allocator : dispose;
+        dispose!(A, T)(alloc, p);
+    }
+}
+
+void disposeX(A, T)(auto ref A alloc, auto ref T p)
+if (is(T == class) || is(T == interface))
+{
+    version(safeAlloc)
+    {
+    }
+    else
+    {
+        import stdx.allocator : dispose;
+        dispose!(A, T)(alloc, p);
+    }
+}
+
+void disposeX(A, T)(auto ref A alloc, auto ref T[] array)
+{
+    version(safeAlloc)
+    {
+    }
+    else
+    {
+        import stdx.allocator : dispose;
+        dispose!(A, T)(alloc, p);
+    }
+}
