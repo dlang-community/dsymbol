f5e6322cb264aaf189cb6eb4649805512d825781
Per Nordlöw <per.nordlow@gmail.com>
2021-08-25 23:39:29 +0200

    Use symbolic import of theAllocator

--- src/dsymbol/tests.d
+++ src/dsymbol/tests.d
@@ -1,6 +1,6 @@
 module dsymbol.tests;
 
-import stdx.allocator;
+import stdx.allocator : theAllocator;
 import dparse.ast, dparse.parser, dparse.lexer, dparse.rollback_allocator;
 import dsymbol.cache_entry, dsymbol.modulecache, dsymbol.symbol;
 import dsymbol.conversion, dsymbol.conversion.first, dsymbol.conversion.second;
